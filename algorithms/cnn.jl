80180ad045c2d1ad83c46a07489b58d1b25962a1
thomashopkins32 <thomashopkins000@gmail.com>
2022-10-15 11:15:22 -0400

    Finished CNN implementation; Started trying to train

--- algorithms/cnn.jl
+++ algorithms/cnn.jl
@@ -1,6 +1,6 @@
+using FLux
+using MLDatasets
 
-
-# TODO: Add assertions for known invariants and test result on MNIST
 function conv2d(in_channels::Int, out_channels::Int, kernel_size::Tuple{Int, Int}; stride::Int = 1)
     W = rand(Float32, in_channels, out_channels, kernel_size[1], kernel_size[2])
     b = rand(Float32, in_channels, out_channels)
@@ -15,13 +15,19 @@ function conv2d(in_channels::Int, out_channels::Int, kernel_size::Tuple{Int, Int
         res = zeros(Float32, (out_dim1, out_dim2, out_channels, size(x, 4)))
         for i = 1:in_channels, j = 1:out_channels
             # iterate over patches of the image using kernel size and stride
-            for s1 = 1:dim1:stride, s2 = 1:dim2:stride
-                # sum over element wise product and add bias
-                r = sum(W[i, j] .* x[s1:k1, s2:k2, i, :], 1) .+ b[i, j]
-                # store result
-                res[s1, s2, j, :] = r
+            out1 = 1
+            for s1 = 1:dim1:stride
+                out2 = 1
+                for s2 = 1:dim2:stride
+                    # sum over element-wise product and store result
+                    res[out1, out2, j, :] = sum(W[i, j] .* x[s1:s1 + k1, s2:s2 + k2, i, :]; dims=(1, 2)) .+ b[i, j]
+                    out2 += 1
+                end
+                out1 += 1
             end
         end
+        return res
     end
     return forward
 end
+
