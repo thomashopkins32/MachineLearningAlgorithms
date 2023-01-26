Removed Encoder, fixed masking

algorithms/transformer.py
@@ -3,11 +3,15 @@ import torch.nn as nn
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, kdim):
+    def __init__(self, mdim, kdim, vdim):
         super().__init__()
         self.kdim = kdim
+        self.q_proj = nn.Linear(mdim, kdim, bias=False)
+        self.k_proj = nn.Linear(mdim, kdim, bias=False)
+        self.v_proj = nn.Linear(mdim, vdim, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(kdim, kdim))) 
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v):
         '''
         Parameters
         ----------
@@ -17,13 +21,13 @@ class SelfAttention(nn.Module):
             Keys of dim (batch size, sequence length, kdim)
         v : torch.Tensor
             Values of dim (batch size, sequence length, vdim)
-        mask : torch.Tensor, optional
-            Mask to apply to avoid information leakage from the future
         '''
-        x = q @ k.T 
-        x = x / torch.sqrt(self.kdim)
-        if mask is not None:
-            x[mask] = -torch.inf
+        q = self.q_proj(q)
+        k = self.k_proj(k)
+        v = self.v_proj(v)
+        x = q @ k.transpose(-2, -1)
+        x = x * self.kdim ** -0.5
+        x.masked_fill(self.tril[:q.shape[1], :q.shape[1]] == 0, -torch.inf)
         x = torch.softmax(x, dim=1)
         return x @ v
 
@@ -32,36 +36,15 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, nheads, mdim, kdim, vdim):
         super().__init__()
         self.nheads = nheads
-        self.attention = SelfAttention(kdim)
+        self.attention = nn.ModuleList((SelfAttention(mdim, kdim, vdim) for _ in range(nheads)))
         self.out = nn.Linear(vdim * nheads, mdim, bias=False)
 
-    def forward(self, q, k, v, mask=None):
-        x = self.attention(q, k, v, mask=mask)
-        return self.out(x)
-
-
-class TransformerEncoderBlock(nn.Module):
-    def __init__(self, nheads, mdim, kdim, vdim, ffdim, sqlength):
-        super().__init__()
-        self.multi_attention = MultiHeadAttention(nheads, mdim, kdim, vdim)
-        self.ln1 = nn.LayerNorm((sqlength, mdim))
-
-        self.l1 = nn.Linear(mdim, ffdim)
-        self.relu = nn.ReLU()
-        self.l2 = nn.Linear(ffdim, mdim)
-        self.ln2 = nn.LayerNorm((sqlength, mdim))
-    
-    def forward(self, x):
-        o = self.multi_attention(x, x, x, mask=None)
-        x = o + x
-        x = self.ln1(x)
-
+    def forward(self, q, k, v):
         outs = []
-        for i in range(x.shape[1]):
-            outs.append(self.l2(self.relu(self.l1(x[:, i, :]))))
-        o = torch.stack(outs, dim=1)
-        x = o + x
-        return self.ln2(x)
+        for n in range(self.nheads):
+            outs.append(self.attention[n](q, k, v))
+        x = torch.stack(outs, dim=1)
+        return self.out(x)
 
 
 class TransformerDecoderBlock(nn.Module):
@@ -69,44 +52,20 @@ class TransformerDecoderBlock(nn.Module):
         super().__init__()
         self.multi_attention1 = MultiHeadAttention(nheads, mdim, kdim, vdim)
         self.ln1 = nn.LayerNorm((sqlength, mdim))
-        
-        self.multi_attention2 = MultiHeadAttention(nheads, mdim, kdim, vdim)
-        self.ln2 = nn.LayerNorm((sqlength, mdim))
 
         self.l1 = nn.Linear(mdim, ffdim)
         self.relu = nn.ReLU()
         self.l2 = nn.Linear(ffdim, mdim)
         self.ln2 = nn.LayerNorm((sqlength, mdim))
     
-    def forward(self, x, enc):
-        o = self.multi_attention1(x, x, x, mask=torch.triu_indices(x.shape[1], x.shape[2]))
+    def forward(self, x):
+        o = self.multi_attention1(x, x, x)
         x = o + x
         x = self.ln1(x)
 
-        o = self.multi_attention2(enc, enc, x)
-        x = o + x
-        x = self.ln2(x)
-
-        outs = []
-        for i in range(x.shape[1]):
-            outs.append(self.l2(self.relu(self.l1(x[:, i, :]))))
-        o = torch.stack(outs, dim=1)
+        o = self.l2(self.relu(self.l1(x)))
         x = o + x
         return self.ln2(x)
-
-
-class TransformerEncoder(nn.Module):
-    def __init__(self, n_layers, nheads, mdim, kdim, vdim, ffdim, sqlength):
-        super().__init__()
-        self.encoders = nn.ModuleList([
-            TransformerEncoderBlock(nheads, mdim, kdim, vdim, ffdim, sqlength)
-            for _ in range(n_layers)
-        ])
-
-    def forward(self, x):
-        for i in range(len(self.encoders)):
-            x = self.encoders[i](x)
-        return x
 
 
 class TransformerDecoder(nn.Module):
@@ -117,9 +76,9 @@ class TransformerDecoder(nn.Module):
             for _ in range(n_layers)
         ])
     
-    def forward(self, x, enc):
+    def forward(self, x):
         for i in range(len(self.decoders)):
-            x = self.decoders[i](x, enc)
+            x = self.decoders[i](x)
         return x
 
 
@@ -127,9 +86,7 @@ class PositionalEncoder(nn.Module):
     def __init__(self, mdim, max_length):
         super().__init__()
         position = torch.arange(max_length).unsqueeze(1)
-        #print(f'position: {position.shape}')
         self.encoder = torch.zeros(1, max_length, mdim)
-        #print(f'func: {torch.sin(position / (10000 ** ((2 * torch.arange(0, mdim, 2)) / mdim))).shape}')
         self.encoder[0, :, 0::2] = torch.sin(position / (10000 ** ((2 * torch.arange(0, mdim, 2)) / mdim)))
         self.encoder[0, :, 1::2] = torch.cos(position / (10000 ** ((2 * torch.arange(0, mdim, 2)) / mdim)))
 
@@ -138,14 +95,13 @@ class PositionalEncoder(nn.Module):
 
 
 class Transformer(nn.Module):
+    ''' Decoder only since I am not doing translation '''
     def __init__(self, vocab_size, n_layers, nheads, mdim, kdim, vdim, ffdim, sqlength):
         super().__init__()
         self.in_embeds = nn.Embedding(vocab_size, mdim)
-        self.out_embeds = nn.Embedding(vocab_size, mdim)
 
         self.pos_encoder = PositionalEncoder(mdim, sqlength)
 
-        self.encoder = TransformerEncoder(n_layers, nheads, mdim, kdim, vdim, ffdim, sqlength)
         self.decoder = TransformerDecoder(n_layers, nheads, mdim, kdim, vdim, ffdim, sqlength)
 
         self.out = nn.Linear(mdim, vocab_size)
@@ -157,17 +113,7 @@ class Transformer(nn.Module):
         print(f'i_emb1: {i_emb.shape}')
         i_emb = self.pos_encoder(i_emb).squeeze()
         print(f'i_emb2: {i_emb.shape}')
-
-        outputs = x[:, 1:, :]
-        print(f'outputs: {outputs.shape}')
-        o_emb = self.out_embeds(outputs).squeeze()
-        print(f'o_emb1: {o_emb.shape}')
-        o_emb = self.pos_encoder(o_emb).squeeze()
-        print(f'o_emb2: {o_emb.shape}')
-
-        enc = self.encoder(i_emb)
-        print(f'enc: {enc.shape}')
-        x = self.decoder(o_emb, enc)
+        x = self.decoder(i_emb)
         print(f'x: {x.shape}')
         return self.out(x)
 