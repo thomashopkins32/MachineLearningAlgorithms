8936438d331750e81179046be553eb8fb431570e
Thomas Hopkins <thomashopkins000@gmail.com>
2024-06-23 14:46:32 -0400

    Runnable icm.py

--- algorithms/icm.py
+++ algorithms/icm.py
@@ -7,7 +7,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import gymnasium
-from tqdm import tqdm
+from tqdm import tqdm # type: ignore
+import matplotlib.pyplot as plt
 
 
 class TrajectoryBuffer:
@@ -27,10 +28,18 @@ class TrajectoryBuffer:
         self.gae_discount_factor = gae_discount_factor
         self.observations: list[torch.Tensor] = []
         self.next_observations: list[torch.Tensor] = []
-        self.actions: list[torch.Tensor] = []
+        self.actions: list[int] = []
         self.rewards: list[float] = []
         self.values: list[float] = []
         self.log_probs: list[torch.Tensor] = []
+
+    def reset(self):
+        self.observations = []
+        self.next_observations = []
+        self.actions = []
+        self.rewards = []
+        self.values = []
+        self.log_probs = []
 
     def store(
         self,
@@ -41,8 +50,8 @@ class TrajectoryBuffer:
         value: float,
         log_prob: torch.Tensor,
     ) -> None:
-        if len(self.observations) == self.max_buffer_size:
-            logging.warn(
+        if self.is_full():
+            logging.warning(
                 f"Cannot store additional time-steps in an already full trajectory. Current size: {len(self.observations)}. Max size: {self.max_buffer_size}"
             )
             return
@@ -54,10 +63,9 @@ class TrajectoryBuffer:
         self.log_probs.append(log_prob)
 
     def get(self, last_value: float) -> dict[str, torch.Tensor]:
-        size = len(self.observations)
-        if size < self.max_buffer_size:
-            logging.warn(
-                f"Computing information on a potentially unfinished trajectory. Current size: {size}. Max size: {self.max_buffer_size}"
+        if not self.is_full():
+            logging.warning(
+                f"Computing information on a potentially unfinished trajectory. Current size: {len(self.observations)}. Max size: {self.max_buffer_size}"
             )
         observations = torch.stack(self.observations)
         next_observations = torch.stack(self.observations)
@@ -84,11 +92,14 @@ class TrajectoryBuffer:
         return {
             "observations": observations,
             "next_observations": next_observations,
-            "actions": torch.stack(self.actions),
+            "actions": torch.tensor(self.actions, dtype=torch.int64),
             "returns": returns,
             "advantages": advantages,
             "log_probs": torch.stack(self.log_probs),
         }
+
+    def is_full(self) -> bool:
+        return len(self.observations) == self.max_buffer_size
 
 
 class PPO:
@@ -100,10 +111,10 @@ class PPO:
         critic: nn.Module,
         train_actor_iters=80,
         train_critic_iters=80,
-        clip_ratio=0.5,
-        target_kl=0.9,
-        actor_lr=3e-4,
-        critic_lr=1e-3,
+        clip_ratio=0.2,
+        target_kl=0.01,
+        actor_lr=3e-5,
+        critic_lr=1e-4,
     ):
         # Environment & Agent
         self.actor = actor
@@ -135,7 +146,7 @@ class PPO:
 
         action_dist = self.actor(env_obs)
         # TODO: Double check this
-        logp = action_dist.gather(1, act.unsqueeze(-1)).squeeze().log()
+        logp = action_dist.softmax(-1).gather(1, act.unsqueeze(-1)).squeeze().log()
         ratio = torch.exp(logp - logp_old)
         clip_adv = torch.clamp(ratio, 1 - self.clip_ratio, 1 + self.clip_ratio) * adv
         loss = -(torch.min(ratio * adv, clip_adv)).mean()
@@ -161,7 +172,7 @@ class PPO:
             data["observations"],
             data["returns"],
         )
-        _, v = self.critic(env_obs)
+        v = self.critic(env_obs)
         return ((v - ret) ** 2).mean()
 
     def _update_critic(self, data):
@@ -180,7 +191,7 @@ class PPO:
 
 
 class ICM:
-    def __init__(self, phi, forward_model, inverse_model, eta=1.0):
+    def __init__(self, phi, forward_model, inverse_model, eta=10.0):
         # feature extractor
         self.phi = phi
 
@@ -209,7 +220,6 @@ class ICM:
             loss = self.eta * torch.nn.functional.mse_loss(xt_1_pred, xt_1)
 
         return loss.item()
-        
 
     def forward_dynamics(self, data):
         self.forward_optim.zero_grad()
@@ -224,7 +234,7 @@ class ICM:
         xt_1 = self.phi(next_observations)
 
         # forward dyanmics
-        xt_1_pred = self.forward_model(xt, actions)
+        xt_1_pred = self.forward_model(xt, actions.unsqueeze(-1))
 
         # forward loss
         loss = torch.nn.functional.mse_loss(xt_1_pred, xt_1)
@@ -246,10 +256,10 @@ class ICM:
         xt_1 = self.phi(next_observations)
 
         # inverse dynamics
-        a_pred = self.inverse_model(xt, xt_1)
+        a_pred_dist = self.inverse_model(xt, xt_1).softmax(-1)
 
         # inverse loss
-        loss = torch.nn.functional.l1_loss(a_pred, actions)
+        loss = torch.nn.functional.cross_entropy(a_pred_dist, actions)
 
         loss.backward()
 
@@ -327,42 +337,77 @@ def discount_cumsum(x: np.ndarray, discount: float) -> np.ndarray:
 
 if __name__ == "__main__":
     env = gymnasium.make("CartPole-v1")
-    input_shape = env.observation_space.n
-    num_actions = env.action_space.n
-    num_steps = 10_000
+    input_shape = env.observation_space.shape[0]  # type: ignore
+    num_actions = env.action_space.n  # type: ignore
+    num_steps = 1000000
 
     trajectory_buffer = TrajectoryBuffer(
-        500, discount_factor=0.99, gae_discount_factor=0.95
+        10_000, discount_factor=0.99, gae_discount_factor=0.95
     )
 
     ppo_actor = CartPoleActor(input_shape, num_actions)
     ppo_critic = CartPoleCritic(input_shape)
-    inverse_dynamics = CartPoleInverseDynamics(input_shape, num_actions)
-    forward_dynamics = CartPoleForwardDynamics(input_shape)
+    inverse_dynamics = CartPoleInverseDynamics(512, num_actions)
+    forward_dynamics = CartPoleForwardDynamics(512)
     feature_extractor = CartPoleFeatureExtractor(input_shape)
 
     ppo = PPO(ppo_actor, ppo_critic)
     icm = ICM(feature_extractor, forward_dynamics, inverse_dynamics)
 
-    obs = env.reset()
+    env_rewards = []
+    intrinsic_rewards = []
+
+    obs, _ = env.reset()
+    obs = torch.from_numpy(obs).float()
+    episode_env_reward = 0.0
+    episode_intrinsic_reward = 0.0
     for step in tqdm(range(num_steps)):
-        obs = preprocess_observation(obs)
         # Get the action from the model
         with torch.no_grad():
-            action_dist = ppo_actor(obs).softmax()
+            action_dist = ppo_actor(obs).softmax(-1)
             value = ppo_critic(obs).item()
         action = torch.multinomial(action_dist, 1).item()
         logp = action_dist[action].log()
 
         # Take the action and get the next observation
-        next_obs, reward, done, _, _ = env.step(action)
+        next_obs, env_reward, done, _, _ = env.step(action) # type: ignore
         if done:
-            next_obs = env.reset()
-        next_obs = preprocess_observation(next_obs)
+            next_obs, _ = env.reset()
+        next_obs = torch.from_numpy(next_obs).float()
 
         # Add intrinsic reward to the environment reward
-        reward += icm.intrinsic_reward(obs, next_obs, action)
+        intrinsic_reward = icm.intrinsic_reward(obs, next_obs, torch.tensor(action).unsqueeze(0))
+
+        reward = env_reward + intrinsic_reward
+
+        # Get and store episode results
+        episode_env_reward += env_reward
+        episode_intrinsic_reward += intrinsic_reward
+        if done:
+            env_rewards.append(episode_env_reward)
+            intrinsic_rewards.append(episode_intrinsic_reward)
+            episode_env_reward = 0.0
+            episode_intrinsic_reward = 0.0
 
         # Store the transition in the replay buffer
-        trajectory_buffer.store(obs, next_obs, action, reward, value, logp)
+        trajectory_buffer.store(obs, next_obs, action, reward, value, logp) # type: ignore
+        obs = next_obs
 
+        if trajectory_buffer.is_full():
+            last_value = ppo_critic(obs).item()
+            data = trajectory_buffer.get(last_value)
+            ppo.update(data)
+            icm.forward_dynamics(data)
+            icm.inverse_dynamics(data)
+            trajectory_buffer.reset()
+
+    step_range = np.arange(len(env_rewards))
+    rewards = [env_rewards[i] + intrinsic_rewards[i] for i in range(len(env_rewards))]
+    plt.plot(step_range, rewards, label='total')
+    plt.plot(step_range, intrinsic_rewards, label="intrinsic reward")
+    plt.plot(step_range, env_rewards, label='environment reward')
+    plt.legend()
+    plt.show()
+
+# TODO: Random minibatches from the full trajectory
+# TODO: Run ppo.py in a separate file first before adding ICM. We need to verify that PPO works before adding ICM
